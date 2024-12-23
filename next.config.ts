11e267a9d3df89f19f058a652c482b1e5f961992
david mosca <david.mosca69@gmail.com>
2024-12-23 02:05:39 +0100

    add scss files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    prependData: `
+    @import "src/styles/_main.scss";
+  `,
+  },
 };
 
 export default nextConfig;
